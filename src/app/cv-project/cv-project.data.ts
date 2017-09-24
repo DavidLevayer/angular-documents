e3031da4d7ab46a7a404ba170377c9219a2d9911
DavidLevayer <david.levayer@gmail.com>
2017-09-24 11:17:51 +0200

    Add example with a list

--- src/app/cv-project/cv-project.data.ts
+++ src/app/cv-project/cv-project.data.ts
@@ -33,7 +33,11 @@ export var projects: Project[] = [
     '@ AwesomeCorp',
     '2016 - 2017',
     'Enthousiast developer',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris:' +
+    '<ul>' +
+    '<li>nisi ut aliquip ex ea commodo consequat.</li>' +
+    '<li>Duis aute irure dolor in reprehenderit in voluptate</li>' +
+    '</ul>',
     ['Awesome tech', 'Git', 'Responsive', 'Wow!'],
     5,
     'https://github.com/'
