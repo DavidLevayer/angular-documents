057f57f3e292099c6f1ab143dedf6ece929c4a6d
DavidLevayer <david.levayer@gmail.com>
2017-09-24 11:33:59 +0200

    Allow to add highlighted messages

--- src/app/cv-project/cv-project.data.ts
+++ src/app/cv-project/cv-project.data.ts
@@ -24,6 +24,7 @@ export var projects: Project[] = [
     'Since August 2017',
     'Tech lead',
     '<b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    '',
     ['Awesome tech', 'Git', 'Responsive', 'Wow!'],
     3,
     'https://github.com/'
@@ -38,6 +39,7 @@ export var projects: Project[] = [
     '<li>nisi ut aliquip ex ea commodo consequat.</li>' +
     '<li>Duis aute irure dolor in reprehenderit in voluptate</li>' +
     '</ul>',
+    'Wow, this project looks trully awesome!',
     ['Awesome tech', 'Git', 'Responsive', 'Wow!'],
     5,
     'https://github.com/'
@@ -48,6 +50,7 @@ export var projects: Project[] = [
     '2015',
     'Enthousiast developer',
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    '',
     ['Awesome tech', 'Git', 'Responsive', 'Wow!'],
     3
   )
--- src/app/cv-project/project/project.ts
+++ src/app/cv-project/project/project.ts
@@ -4,17 +4,19 @@ export class Project {
   duration: string;
   position: string;
   description: string;
+  highlight: string;
   keywords: string[];
   teamSize: number;
   github: string;
 
   constructor(title?: string, client?: string, duration?: string, position?: string, description?: string,
-    keywords?: string[], teamSize?: number, github?: string) {
+    highlight?: string, keywords?: string[], teamSize?: number, github?: string) {
       this.title = title;
       this.client = client;
       this.duration = duration;
       this.position = position;
       this.description = description;
+      this.highlight = highlight;
       this.keywords = keywords;
       this.teamSize = teamSize;
       this.github = github;
