e774f6f2392b3b09939a055915cd95238926b38f
DavidLevayer <david.levayer@gmail.com>
2017-09-24 11:16:02 +0200

    Allow HTML tags in project description

--- src/app/cv-project/cv-project.data.ts
+++ src/app/cv-project/cv-project.data.ts
@@ -23,7 +23,7 @@ export var projects: Project[] = [
     'Personnal',
     'Since August 2017',
     'Tech lead',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    '<b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     ['Awesome tech', 'Git', 'Responsive', 'Wow!'],
     3,
     'https://github.com/'
